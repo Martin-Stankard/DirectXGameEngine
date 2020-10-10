ef66dff2f5a9c3e957e356b74dc2726f371248ac
Martin Stankard <martin.stankard@hotmail.com>
2020-10-10 10:10:45 -0700

    vertex shader now creates a parallelogram

--- DirectXGame/VertexShader.hlsl
+++ DirectXGame/VertexShader.hlsl
@@ -7,5 +7,9 @@ float4 vsmain(float4 position : POSITION) : SV_POSITION
 	if (position.y > -1 && position.y < 0 && position.x > 0 &&position.x < 1) {
 		position.y += .25f;
 	}
+
+	if (position.y > 0 && position.y < 1 && position.x > -1 && position.x < 0) {
+		position.y -= .25f;
+	}
 	return position;
 }
